36e4035d4372008dc13a3ab3821ffb74fe221392
Chun Shiang Liu <liucy070@mymail.unisa.edu.au>
2023-06-17 15:17:51 +0930

    webpage

--- Assignment_3_WebPage/script.js
+++ Assignment_3_WebPage/script.js
@@ -1,40 +1,23 @@
-const mode = "light";
-const name = "";
-
-const toggleMode = () => {
-if (mode === "light") {
-mode = "dark";
-} else {
-mode = "light";
-}
-
-document.getElementById("mode-toggle").innerHTML = `
-<button id="light-mode">Light Mode</button>
-<button id="dark-mode">Dark Mode</button>
-`;
-
-document.querySelector(`[id="${mode}-mode"`).classList.add("active");
-document.querySelector(`[id="${!mode}-mode"`).classList.remove("active");
-};
-
-const getName = () => {
-name = document.getElementById("name").value;
-};
-
-const getDateAndTime = () => {
-const date = new Date();
-const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-const timeString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-
-document.getElementById("date-time").innerHTML = `
-Current Date and Time: ${dateString} ${timeString}
-`;
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-getName();
-getDateAndTime();
-
-document.getElementById("light-mode").addEventListener("click", toggleMode);
-document.getElementById("dark-mode").addEventListener("click", toggleMode);
-});
+// Function to toggle the theme
+function toggleTheme() {
+    const root = document.querySelector(':root');
+    const currentTheme = root.getAttribute('data-theme');
+  
+    // Toggle the theme attribute
+    if (currentTheme === 'light') {
+      root.setAttribute('data-theme', 'dark');
+    } else {
+      root.setAttribute('data-theme', 'light');
+    }
+  }
+  
+  // Function to display current date and time
+  function displayDateTime() {
+    const currentDate = new Date();
+    const dateTimeElement = document.getElementById('datetime');
+    dateTimeElement.textContent = currentDate.toLocaleString();
+  }
+  
+  // Run the displayDateTime function when the page loads
+  window.addEventListener('load', displayDateTime);
+  
