d05009d872f37f666dfa6151f953aee8fa4369bf
Chun Shiang Liu <liucy070@mymail.unisa.edu.au>
2023-06-17 20:47:29 +0930

    js

--- Assignment_3_WebPage/script.js
+++ Assignment_3_WebPage/script.js
@@ -1,83 +1,72 @@
-function toggleMode() {
-  var body = document.body;
-  body.classList.toggle("dark-mode");
+// Get elements
+const modeButton = document.getElementById("modeButton");
+const nameInput = document.getElementById("nameInput");
+const heading = document.getElementById("heading");
+const dateTime = document.getElementById("dateTime");
+
+// Check local storage for saved data
+if (localStorage.getItem("mode") === "dark") {
+    enableDarkMode();
 }
 
-// Function to update the heading with the entered name
-function updateName() {
-  var name = document.getElementById("name-input").value;
-  document.getElementById("heading").textContent = "Welcome, " + name + "!";
-}
-
-// Function to display the current date and time
-function displayDateTime() {
-  var dateTime = new Date();
-  document.getElementById("date-time").textContent = "Current Date and Time: " + dateTime;
-}
-
-// Check if there is a saved name in local storage and update the heading
-var savedName = localStorage.getItem("name");
+const savedName = localStorage.getItem("name");
 if (savedName) {
-  document.getElementById("name-input").value = savedName;
-  updateName();
+    nameInput.value = savedName;
+    heading.textContent = `Welcome, ${savedName}!`;
 }
 
-// Add event listeners for the button and input field
-document.getElementById("toggle-button").addEventListener("click", toggleMode);
-document.getElementById("name-input").addEventListener("input", updateName);
+// Event listeners
+modeButton.addEventListener("click", toggleMode);
+nameInput.addEventListener("input", updateName);
 
-// Call the displayDateTime function to show the current date and time
+// Function to toggle dark mode
+function toggleMode() {
+    if (document.body.classList.contains("dark-mode")) {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+}
+function toggleTheme() {
+  const root = document.documentElement;
+  const currentTheme = root.getAttribute('data-theme');
+  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+  root.setAttribute('data-theme', newTheme);
+}
+
+function enableDarkMode() {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("mode", "dark");
+}
+
+function disableDarkMode() {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("mode", "light");
+}
+
+// Function to update name
+function updateName() {
+    const newName = nameInput.value;
+    heading.textContent = `Welcome, ${newName}!`;
+    localStorage.setItem("name", newName);
+}
+
+// Function to update date and time
+function updateTime() {
+    const now = new Date();
+    dateTime.textContent = now.toLocaleString();
+}
+
+// Update date and time every second
+setInterval(updateTime, 1000);
+
+function displayDateTime() {
+  const dateTimeElement = document.getElementById('datetime');
+  const now = new Date();
+  const dateTimeString = now.toLocaleString();
+  dateTimeElement.textContent = dateTimeString;
+}
+
+// Call the function to display the date and time when the page loads
 displayDateTime();
 
-// Function to change the displayed image and information
-function changeImage(button) {
-  var image = document.getElementById("main-image");
-  var info = document.getElementById("image-info");
-
-  // Map button text to corresponding image URL and information
-  var imageMap = {
-    "Japanese Garden Tree": {
-      url: "https://www.qa-systems.com/blog/software-drives-advances-in-medical-technology/",
-      info: 'Software Drives Advances in Medical Technology'
-    },
-    "Ceiba Tree": {
-      url: "https://openverse.org/image/bc3938ee-5cdd-48f5-a808-a8d441904bd3?q=trees",
-      info: '"Ceiba Tree" by LollyKnit is licensed under CC BY 2.0. To view a copy of this license, visit <a href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">https://creativecommons.org/licenses/by/2.0/?ref=openverse</a>.'
-    },
-    "Ancient Olive Trees": {
-      url: "https://openverse.org/image/f1230075-5ab6-45c3-a027-87bf0874a714?q=trees",
-      info: '"Corfu - Sept 2005 - Ancient Olive Trees" by Gareth1953 All Right Now
-    },
-    "Palm tree": {
-      url: "https://openverse.org/image/b7313e95-498b-4917-9a2b-7fd50f072bfe?q=trees",
-      info: '"Palm tree" by F H Mira is licensed under CC BY-SA 2.0. To view a copy of this license, visit <a href="https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse">https://creativecommons.org/licenses/by-sa/2.0/?ref=openverse</a>.'
-    },
-    "Mangroves": {
-      url: "https://openverse.org/image/752085e7-81bb-473c-a0fc-2f19fa977345?q=mangrove",
-      info: '"Mangrove" by Brisbane City Council is licensed under CC BY 2.0. To view a copy of this license, visit <a href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">https://creativecommons.org/licenses/by/2.0/?ref=openverse</a>.'
-    }
-  };
-
-  // Get the image URL and information corresponding to the clicked button
-  var buttonText = button.textContent;
-  var imageData = imageMap[buttonText];
-
-  // Update the image source and information
-  image.src = imageData.url;
-  info.innerHTML = imageData.info;
-  function updateHeadingFromQueryString() {
-    // Get the query string from the URL
-    const queryString = window.location.search;
-  
-    // Create a URLSearchParams object to parse the query string
-    const urlParams = new URLSearchParams(queryString);
-  
-    // Get the value of the 'user_name' parameter from the query string
-    const userName = urlParams.get('user_name');
-  
-    // Get the heading element by its ID
-    const headingElement = document.getElementById('heading');
-  
-    // Update the heading with the user's name
-    headingElement.textContent = `Hello, ${userName}`;
-  }
